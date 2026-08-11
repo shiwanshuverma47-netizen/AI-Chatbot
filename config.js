2e516410e389e4d5f128d392b6aa6891e94a906d
Shiwanshu Verma <shiwanshuverma47@gmail.com>
2026-08-11 07:43:01 +0530

    config: add study configuration defaults

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.MBA_BUDDY_CONFIG = {
   themeKey: 'mba-buddy-theme',
-  defaultTheme: 'light'
+  defaultTheme: 'light',
+  studyFocusWindow: 'today',
+  defaultStudyPlan: 'Review one concept, one formula, and one example'
 };
